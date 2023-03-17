986621315f60d292f1232b1b4404902f252d377d
jparraporcar <jordiparraporcar@gmail.com>
2023-03-17 21:00:23 +0900

    data being sent to frontend but error type: system, errno: ENOTFOUND, CODE: ENOTFOUND appears from binance, need to handle

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import express, { Request, Response } from 'express'
 import { GetPriceVolumeData } from './Controllers/GetPriceVolumeData'
 const app = express()
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+// eslint-disable-next-line no-var, @typescript-eslint/no-var-requires
+var cors = require('cors')
+
+app.use(cors())
 
 app.get('/priceVolumeData', async (req: Request, res: Response) => {
+    const today = new Date()
+    const time =
+        today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+    console.log(`request received at time ${time}`)
     const data = await GetPriceVolumeData.handle(req, res)
     res.send(data)
 })
