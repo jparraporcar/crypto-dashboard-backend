f22d7227ef898a10c23c2ab366a967ada0a95b5b
jparraporcar <jordiparraporcar@gmail.com>
2023-04-07 13:37:59 +0900

    added new error classes and logic to assess if existing user when dynamodb put action - 1

--- .build/src/client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.client = void 0;
-const binance_api_node_1 = __importDefault(require("binance-api-node"));
-exports.client = (0, binance_api_node_1.default)();
-//# sourceMappingURL=client.js.map
--- .build/src/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const envVars = Object.assign({ 
-    // default values
-    STAGE: 'dev', AWS_DEFAULT_REGION: 'ap-northeast-1' }, process.env);
-exports.default = Object.freeze(envVars);
-//# sourceMappingURL=env.js.map
